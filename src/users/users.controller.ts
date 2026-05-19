74903b9f215ae1970301ff66a5f6824bf8337d10
Adnan <adnan.revnix@gmail.com>
2026-05-19 16:23:06 +0500

    add users module and controller with basic CRUD operations

---
+++ src/users/users.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Body,
+  Headers,
+  Ip,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  public getAllUsers(@Query() query: any): string {
+    console.log(query);
+    return `This action returns all users with query: ${JSON.stringify(query)}`;
+  }
+
+  @Get(':id')
+  @Get(':id/:optional')
+  public getUsers(
+    @Param('id') id: any,
+    @Param('optional') optional: any,
+    @Query() query: any,
+  ): string {
+    console.log(id);
+    console.log(optional);
+    console.log(query);
+    return `This action returns user with ID: ${id} optional: ${optional} with query: ${JSON.stringify(query)}`;
+  }
+
+  @Post()
+  public createUser(
+    @Body() request: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ): string {
+    console.log(request);
+    console.log(headers);
+    console.log(ip);
+    return 'This action creates a new user';
+  }
+}
